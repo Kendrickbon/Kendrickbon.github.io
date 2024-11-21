2bf63b73616c1b6b6a2de1da9a4123d38832d521
Kendrickbon <kendrickbonneejr17@icloud.com>
2024-11-21 00:38:29 +0000

    add index.html and css files

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -38,17 +38,20 @@ $(function () {
     // Create platforms
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
-
-
-
-    
+    createPlatform(700, 400, 200, 20);
+    createPlatform(500, 570, 200, 20);
+    createPlatform(400, 600, 200, 20);
+    createPlatform(200, 700, 200, 20);
+    createPlatform(100, 750, 200, 20);
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
+    createCollectable("steve", 500, 300, 20, 0.5);
+    createCollectable("max", 450, 250);
+    createCollectable("grace", 350, 200);
+    createCollectable("kennedi", 250, 150);
 
     
     // TODO 3
@@ -56,9 +59,9 @@ $(function () {
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
+createCannon("right", 600, 1000);
+createCannon("right", 500, 1500);
+createCannon("right", 400, 2000);
     
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
@@ -67,5 +70,3 @@ $(function () {
 
   registerSetup(setup);
 });
-
-function = createPlatform(500, 300, 200, 20)
